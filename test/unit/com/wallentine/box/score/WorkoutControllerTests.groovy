9eb12911edd6e4fb57b2bc411511036cc7f94b92
Todd Wallentine <todd@theideacenter.org>
2013-04-06 20:24:23 -0500

    Auto-generated tests. These need to be updated to have real test logic.

---
+++ test/unit/com/wallentine/box/score/WorkoutControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.wallentine.box.score
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(WorkoutController)
+class WorkoutControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
---
+++ test/unit/com/wallentine/box/score/WorkoutTests.groovy
@@ -0,0 +1,17 @@
+package com.wallentine.box.score
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Workout)
+class WorkoutTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
