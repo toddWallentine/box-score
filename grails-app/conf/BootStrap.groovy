748f358b0c1335c341aee3dd10d42f1f4e25acae
Todd Wallentine <todd@theideacenter.org>
2013-05-01 17:40:46 -0500

    Updated the bootstrap so it won't add new test data if data already exists.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,16 +9,36 @@ class BootStrap {
             case "development":
             case "test":
             case "production": // comment out when going live so we don't bootstrap in production.
-                def MAX_TEAMS = 5
-                def MAX_ATHLETES = 10
-                for(int i = 0; i < MAX_TEAMS; i++) {
-                    def team = new Team(name: "Team ${i}")
-                    for(int j = 0; j < MAX_ATHLETES; j++) {
-                        def gender = (j % 2 == 0) ? "M" : "F"
-                        def athlete = new Athlete(name: "Athlete ${i}${j}", gender: gender)
-                        team.addToAthletes(athlete)
+
+                if(Workout.count() <= 0) {
+                    // bootstrap some test data if none exists.
+                    new Workout(number: 1, name: "Killer").save()
+                    new Workout(number: 2, name: "Tame").save()
+                    new Workout(number: 3, name: "Long").save()
+                }
+
+                if(Athlete.count() <= 0) {
+                    // bootstrap some test data if none exists.
+                    def MAX_TEAMS = 6
+                    def MAX_ATHLETES = 10
+                    for(int i = 0; i < MAX_TEAMS; i++) {
+                        def team = new Team(name: "Team ${i}")
+                        for(int j = 0; j < MAX_ATHLETES; j++) {
+                            def gender = (j % 2 == 0) ? "M" : "F"
+                            def athlete = new Athlete(name: "Athlete ${i}${j}", gender: gender)
+                            team.addToAthletes(athlete)
+                        }
+                        team.save()
                     }
-                    team.save()
+
+                    def random = new java.util.Random()
+                    Athlete.list().each { athlete ->
+                        Workout.list().each { wod ->
+                            def reps = Math.abs(random.nextInt() % 20 + 1)
+                            def score = new Score(reps: reps, athlete: athlete, wod: wod)
+                            score.save()
+                        }
+                    }
                 }
 
                 /*
@@ -54,19 +74,6 @@ class BootStrap {
             	cfmkTeam.addToAthletes(alex)
             	cfmkTeam.save()
                 */
-
-            	new Workout(number: 1, name: "Killer").save()
-            	new Workout(number: 2, name: "Tame").save()
-            	new Workout(number: 3, name: "Long").save()
-
-            	def random = new java.util.Random()
-            	Athlete.list().each { athlete ->
-            		Workout.list().each { wod ->
-                        def reps = Math.abs(random.nextInt() % 20 + 1)
-                        def score = new Score(reps: reps, athlete: athlete, wod: wod)
-                        score.save()
-            		}
-            	}
                 break;
         }
     }
